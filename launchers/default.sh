619dc5d41c45afcad4794b9b22d4a790f572e922
Lorand Cheng <lfcheng@usc.edu>
2022-07-15 16:28:57 -0500

    updating script

--- launchers/default.sh
+++ launchers/default.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xvfb-run -s "-screen 0 1400x900x24" /bin/bash # virtual screen for video recording
+xvfb-run -s "-screen 0 1400x900x24" # virtual screen for video recording
 python3 -m PbMORL.main # launch main program
