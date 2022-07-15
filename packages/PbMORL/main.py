8b029197ed956b5bb547d8c0017fa4246d2db414
Lorand Cheng <lfcheng@usc.edu>
2022-07-15 12:57:14 -0500

    testing imports

--- packages/PbMORL/main.py
+++ packages/PbMORL/main.py
@@ -1,7 +1,15 @@
+from os import environ
 import gym
 from matplotlib import pyplot as plt
 import numpy as np
 from stable_baselines3 import DQN
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import sys
+sys.path.insert(0, './packages/MO-highway-env/')
+import highway_env
 
 if __name__ == "__main__":
     print("Hello from Python")
