ab8c218f23bb621d49eeca5019899d1ce6e97811
Lorand Cheng <lfcheng@usc.edu>
2022-07-15 15:02:33 -0500

    testing more imports

--- packages/PbMORL/main.py
+++ packages/PbMORL/main.py
@@ -10,6 +10,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import sys
 sys.path.insert(0, './packages/MO-highway-env/')
 import highway_env
+from highway_env.scripts.utils import record_videos, show_videos
 
 if __name__ == "__main__":
     print("Hello from Python")
