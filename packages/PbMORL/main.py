629bdab12957cf535a24b3deba801b90d6177c11
Lorand Cheng <lfcheng@usc.edu>
2022-07-20 13:15:31 -0500

    fixing video recorder, centering and resizing

--- packages/PbMORL/main.py
+++ packages/PbMORL/main.py
@@ -1,6 +1,5 @@
-from os import environ
 import gym
-from matplotlib import pyplot as plt
+from gym.wrappers import RecordVideo
 import numpy as np
 from stable_baselines3 import DQN
 
@@ -10,34 +9,36 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import sys
 sys.path.insert(0, './packages/MO-highway-env/')
 import highway_env
-# sys.path.insert(0, './packages/MO-highway-env/scripts/')
-# from utils import record_videos
-
-from gym import wrappers
 
 NUM_TRAJECTORIES = 10
 
 if __name__ == "__main__":
-    model = DQN.load("./packages/PbMORL/models/right_model")
+    model = DQN.load("./packages/PbMORL/models/speed_model")
 
     env = gym.make("mo-highway-v0")
     env.seed(0)
     env.configure({
+        "screen_width": 1400,
+        "screen_height": 200,
+        "centering_position": [0.5, 0.5],
+        "scaling": 5.5,
         "duration": np.Infinity,
-        "cur_reward": 0
+        "cur_reward": 0,
     })
-    # env = record_videos(env, "./packages/PbMORL/videos")
-    env = wrappers.Monitor(env, "./packages/PbMORL/videos", force=True)
+    
+    # Video recording
+    env = RecordVideo(env, video_folder="./packages/PbMORL/videos", episode_trigger=lambda e: True)
+    env.unwrapped.set_record_video_wrapper(env)
 
+    # Run trajectories
     for tau in range(NUM_TRAJECTORIES):
         obs, done = env.reset(), False
-        retrn = 0
+        total_reward = 0
         while not done:
             env.render()
             action, _ = model.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
-            retrn += reward
+            total_reward += reward
         env.render()
-        print(retrn)
-
+        print(total_reward)
     env.close()
