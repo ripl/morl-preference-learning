68cbf1b0e485355172498aa5889702e3e301d762
Lorand Cheng <lfcheng@usc.edu>
2022-07-25 17:18:34 -0500

    fixing image issues

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 # NOTE: this setup script will be executed right before the launcher file inside the container,
 #       use it to configure your environment.
 
